94ce417c8c0a0da79ccfe5ea27364f02b4a952d8
mmaslan <marek.maslany1@gmail.com>
2022-08-05 20:38:19 +0200

    Changes in html - CSS working in about page

--- DjangoBlog/urls.py
+++ DjangoBlog/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +26,5 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('blog.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
