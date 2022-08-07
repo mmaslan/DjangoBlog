025632fc26e5127540f9ad9cd60aa2b76fe3bc09
mmaslan <marek.maslany1@gmail.com>
2022-08-07 15:18:37 +0200

    Delete function

--- blog/urls.py
+++ blog/urls.py
@@ -10,7 +10,7 @@ from .views import (
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-details'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
