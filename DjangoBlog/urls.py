75de98e9e9efad8337de20a45f41a923f9be642c
mmaslan <marek.maslany1@gmail.com>
2022-08-13 17:34:05 +0200

    Password recovery system

--- DjangoBlog/urls.py
+++ DjangoBlog/urls.py
@@ -40,6 +40,11 @@ urlpatterns = [
              template_name='users/password_reset_confirm.html'),
          name='password_reset_confirm'
          ),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'
+         ),
     path('', include('blog.urls')),
 ]
 
