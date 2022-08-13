1e67904a31ec953a49c866533bf4dadbc565339d
mmaslan <marek.maslany1@gmail.com>
2022-08-13 15:23:38 +0200

    Pagination

--- blog/views.py
+++ blog/views.py
@@ -15,6 +15,7 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 3
 
 
 class PostDetailView(DetailView):
