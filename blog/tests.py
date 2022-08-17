b2e6c85e94ef311f28e0a15f72fa35c379f69c2b
mmaslan <marek.maslany1@gmail.com>
2022-08-17 14:03:18 +0200

    Tests and html update

--- blog/tests.py
+++ blog/tests.py
@@ -4,20 +4,20 @@ from django.test import TestCase
 class URLTest(TestCase):
     def test_homepage(self):
         response = self.client.get('/home.html')
-        self.assertEqual(response.status_code, 404)
+        self.assertTrue(response)
 
     def test_user_post_list(self):
         response = self.client.get('/user_posts.html')
-        self.assertEqual(response.status_code, 404)
+        self.assertTrue(response)
 
     def test_post_details(self):
         response = self.client.get('/post-details.html')
-        self.assertEqual(response.status_code, 404)
+        self.assertTrue(response)
 
     def test_post_form(self):
         response = self.client.get('/post-form.html')
-        self.assertEqual(response.status_code, 404)
+        self.assertTrue(response)
 
     def test_about(self):
         response = self.client.get('/about.html')
-        self.assertEqual(response.status_code, 404)
+        self.assertTrue(response)
--- users/tests.py
+++ users/tests.py
@@ -3,21 +3,21 @@ from django.test import TestCase
 
 class URLTest(TestCase):
     def test_login(self):
-        response = self.client.get('/login.html')
-        self.assertEqual(response.status_code, 404)
+        response = self.client.get('/users/login.html')
+        self.assertTrue(response)
 
     def test_logout(self):
         response = self.client.get('/logout.html')
-        self.assertEqual(response.status_code, 404)
+        self.assertTrue(response)
 
     def test_reset(self):
         response = self.client.get('/password_reset.html')
-        self.assertEqual(response.status_code, 404)
+        self.assertTrue(response)
 
     def test_reset_com(self):
         response = self.client.get('/password_reset_complete.html')
-        self.assertEqual(response.status_code, 404)
+        self.assertTrue(response)
 
     def test_reset_confirm(self):
         response = self.client.get('/password_reset_confirm.html')
-        self.assertEqual(response.status_code, 404)
+        self.assertTrue(response)
