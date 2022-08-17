ad3046a1d794ef20a0ecfb28e25cc52eba153a61
mmaslan <marek.maslany1@gmail.com>
2022-08-17 12:25:58 +0200

    Views tests

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class URLTest(TestCase):
+    def test_homepage(self):
+        response = self.client.get('/home.html')
+        self.assertEqual(response.status_code, 404)
+
+    def test_user_post_list(self):
+        response = self.client.get('/user_posts.html')
+        self.assertEqual(response.status_code, 404)
+
+    def test_post_details(self):
+        response = self.client.get('/post-details.html')
+        self.assertEqual(response.status_code, 404)
+
+    def test_post_form(self):
+        response = self.client.get('/post-form.html')
+        self.assertEqual(response.status_code, 404)
+
+    def test_about(self):
+        response = self.client.get('/about.html')
+        self.assertEqual(response.status_code, 404)
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class URLTest(TestCase):
+    def test_login(self):
+        response = self.client.get('/login.html')
+        self.assertEqual(response.status_code, 404)
+
+    def test_logout(self):
+        response = self.client.get('/logout.html')
+        self.assertEqual(response.status_code, 404)
+
+    def test_reset(self):
+        response = self.client.get('/password_reset.html')
+        self.assertEqual(response.status_code, 404)
+
+    def test_reset_com(self):
+        response = self.client.get('/password_reset_complete.html')
+        self.assertEqual(response.status_code, 404)
+
+    def test_reset_confirm(self):
+        response = self.client.get('/password_reset_confirm.html')
+        self.assertEqual(response.status_code, 404)
