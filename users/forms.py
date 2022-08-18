6bfaea749b9df41c79fe24f236ebcff1ea350623
mmaslan <marek.maslany1@gmail.com>
2022-08-18 10:29:22 +0200

    Update

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
